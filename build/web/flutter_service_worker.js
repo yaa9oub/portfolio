d7ace508b31af5c43569d1af8ccf0178ff6e7284
ceyphe-69 <seyf.yagoub.work@gmail.com>
2025-04-16 18:03:28 +0100

    Update service worker version and resource hash for flutter_bootstrap.js

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -39,7 +39,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "2c524c7790f3e93e16a1492f51b42168
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "c2193c1b4ae122164478cf9976334e2c",
+"flutter_bootstrap.js": "02f3f9e2a048e22e7da7ceb9736ef0b5",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
